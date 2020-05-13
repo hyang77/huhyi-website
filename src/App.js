7d4a3d1e98710905c0dd749568d8d468cc8a3dd8
Haoli Yang <hyang77@hawk.iit.edu>
2020-05-13 11:20:21 +0800

    add cotent to App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import logo from "./assets/images/logo.jpg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import HomePage from "./components/HomePage";
+import Footer from "./components/Footer";
+import Product from "./components/Product";
+import Contact from "./components/Contact";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink,
+} from "react-router-dom";
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Router>
+          <div className="App">
+            <header>
+              <img src={logo} className="App-logo" alt="logo" />
+              <div className="companyName">
+                <span>互怡企業</span>
+                <span>HUH-YI Enterprise Co.,Ltd</span>{" "}
+              </div>
+              <nav>
+                <ul>
+                  <li>
+                    <NavLink to="/">
+                      <span>關於互怡</span>
+                      <span>About us</span>
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/product">
+                      <span>產品型錄</span>
+                      <span>Products</span>
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/contact">
+                      <span>聯絡我們</span>
+                      <span>Contact us</span>
+                    </NavLink>
+                  </li>
+                </ul>
+              </nav>
+              <Switch>
+                <Route path="/product">
+                  <Product />
+                </Route>
+                <Route path="/contact">
+                  <Contact />
+                </Route>
+                >
+                <Route path="/">
+                  <HomePage />
+                </Route>
+              </Switch>
+            </header>
+          </div>
+        </Router>
+
+        <HomePage/>
+        <Footer/>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
