133070d31d2bedb65ae75d94aab21372d7364b0a
Haoli Yang <hyang77@hawk.iit.edu>
2020-05-13 11:21:42 +0800

    add content to Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
-
-return null;
+  return (
+    <React.Fragment>
+      <section className="section-container">
+        <div className="footer-info-container">
+          <div>
+            <h3>聯絡我們</h3>
+            <p>huhyitw@gmail.com</p>
+            <p>傳真: 06-2791648, 06-2491580 </p>
+            <p>電話: 06-2791828 </p>
+          </div>
+          <div>
+              <h3>營業時間</h3>
+              <p>早上 8:30 ~ 晚上 5:30 (周一到週五)</p>
+          </div>
+          <div>
+              <h3>公司地址</h3>
+              <p>(717) 台南市仁德區太新路82號</p>
+              <p>查看地圖</p>
+          </div>
+        </div>
+      </section>
+    </React.Fragment>
+  );
 }
 
 export default Footer;
