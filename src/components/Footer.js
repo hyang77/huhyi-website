0ee86c1d181b6b6b9853fc6d2699616e467891a8
Haoli Yang <hyang77@hawk.iit.edu>
2020-05-13 16:37:18 +0800

    add classname

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,20 +3,20 @@ import React from "react";
 function Footer() {
   return (
     <React.Fragment>
-      <section className="section-container">
+      <section className="footer-section-container">
         <div className="footer-info-container">
-          <div>
-            <h3>聯絡我們</h3>
+          <div className="footer-info-details">
+            <p>聯絡我們</p>
             <p>huhyitw@gmail.com</p>
             <p>傳真: 06-2791648, 06-2491580 </p>
             <p>電話: 06-2791828 </p>
           </div>
-          <div>
-              <h3>營業時間</h3>
+          <div className="footer-info-details">
+              <p>營業時間</p>
               <p>早上 8:30 ~ 晚上 5:30 (周一到週五)</p>
           </div>
-          <div>
-              <h3>公司地址</h3>
+          <div className="footer-info-details">
+              <p>公司地址</p>
               <p>(717) 台南市仁德區太新路82號</p>
               <p>查看地圖</p>
           </div>
