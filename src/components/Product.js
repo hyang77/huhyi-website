3714b301675552564e954dcf1e30cc07bac6877e
Haoli Yang <hyang77@hawk.iit.edu>
2020-05-19 08:48:13 +0800

    add sidebar dropdown

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,20 +3,13 @@ import React from "react";
 class Product extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { visibility: false};
-    this.toggleVisibility = this.toggleVisibility.bind(this);
+    this.state = { active: 0 };
+    this.isSelected = this.isSelected.bind(this);
   }
 
-  toggleVisibility() {
-
-    this.setState((state) => {
-      if (state.visibility === true) {
-        return { visibility: false };
-      } else {
-        return { visibility: true };
-      }
-    });
-    console.log(this.state.visibility);
+  isSelected(number) {
+    this.setState({ active: number });
+    console.log(this.state.active);
   }
 
   render() {
@@ -24,18 +17,13 @@ class Product extends React.Component {
       <React.Fragment>
         <nav className="product-nav">
           <ul>
-            <li  id="list1" className="product-sub-menu" onClick={this.toggleVisibility}>
+            <li className="product-sub-menu" onClick={() => this.isSelected(1)}>
               <a href="#">
                 屏風<div className="fa fa-caret-down right"></div>
               </a>
-              <ul
-                
-                style={
-                  this.state.visibility
-                    ? { display: "block" }
-                    : { display: "none" }
-                }
-              >
+              <ul className={`collapsible ${
+                  this.state.active === 1 ? "open" : null
+                }`}>
                 <li>
                   <a href="#">2.5cm屏風</a>
                 </li>
@@ -44,18 +32,13 @@ class Product extends React.Component {
                 </li>
               </ul>
             </li>
-            <li  id="list1" className="product-sub-menu" onClick={this.toggleVisibility}>
+            <li className="product-sub-menu" onClick={() => this.isSelected(2)}>
               <a href="#">
                 椅子系列<div className="fa fa-caret-down right"></div>
               </a>
-              <ul
-                
-                style={
-                  this.state.visibility
-                    ? { display: "block" }
-                    : { display: "none" }
-                }
-              >
+              <ul className={`collapsible ${
+                  this.state.active === 2 ? "open" : null
+                }`}>
                 <li>
                   <a href="#">折合椅</a>
                 </li>
@@ -64,11 +47,15 @@ class Product extends React.Component {
                 </li>
               </ul>
             </li>
-            <li className="product-sub-menu">
+            <li className="product-sub-menu" onClick={() => this.isSelected(3)}>
               <a href="#">
                 桌子系列<div className="fa fa-caret-down right"></div>
               </a>
-              <ul>
+              <ul
+                className={`collapsible ${
+                  this.state.active === 3 ? "open" : null
+                }`}
+              >
                 <li>
                   <a href="#">主管桌</a>
                 </li>
