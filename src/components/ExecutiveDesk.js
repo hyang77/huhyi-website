d8c129694511ca3fcf243b2762ee98cec7af6ead
Haoli Yang <hyang77@hawk.iit.edu>
2020-05-19 17:21:22 +0800

    add importAll method to dynamically import images from folder

--- src/components/ExecutiveDesk.js
+++ src/components/ExecutiveDesk.js
@@ -1,10 +1,28 @@
 import React from "react";
 
 function ExecutiveDesk() {
+  //import images from folder
+  function importAll(r) {
+    return r.keys().map(r);
+  }
+
+  const images = importAll(
+    require.context(
+      "../assets/images/desks/desk-executive",
+      false,
+      /\.(png|jpe?g|svg)$/
+    )
+  );
+  //loading image
+  console.log(images);
+  const loadImages = images.map((image) => {
+    return <img key={image} src={image} alt="" />;
+  });
+
   return (
     <React.Fragment>
-      <div className="executive-desk-title">主管桌</div>
-      <div className="executive-desk-items">這是主管桌產品照片放置區</div>
+      <div className="title executive-desk-title">主管桌</div>
+      <div className="items executive-desk-items">{loadImages}</div>
     </React.Fragment>
   );
 }
