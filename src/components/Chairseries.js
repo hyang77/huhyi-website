c24c5f57ddd975b0f401873be8790ba5726384c7
Haoli Yang <hyang77@hawk.iit.edu>
2020-05-19 11:09:56 +0800

    modify css style

--- src/components/Chairseries.js
+++ src/components/Chairseries.js
@@ -3,8 +3,8 @@ import React from "react";
 function Chairseries() {
   return (
     <React.Fragment>
-      <div className="chair-title">椅子系列</div>
-      <div className="chair-items">這裡顯示所有椅子產品照片(分類劃分)</div>
+      <div className="title chair-title">椅子系列</div>
+      <div className="items chair-items">這裡顯示所有椅子產品照片(分類劃分)</div>
     </React.Fragment>
   );
 }
