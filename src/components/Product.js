7d1959b4bece309c9c2f442a00155349da218765
Haoli Yang <hyang77@hawk.iit.edu>
2020-05-19 11:10:34 +0800

    add English version

--- src/components/Product.js
+++ src/components/Product.js
@@ -24,88 +24,93 @@ class Product extends React.Component {
   render() {
     return (
       <React.Fragment>
-        
-          <nav className="product-nav">
-            <ul>
-              <li
-                className="product-sub-menu"
-                onClick={() => this.isSelected(1)}
-              >
-                <NavLink to="/product/screens">
-                  屏風<div className="fa fa-caret-down right"></div>
-                </NavLink>
-                <ul
-                  className={`collapsible ${
-                    this.state.active === 1 ? "open" : null
-                  }`}
+        <div className="product-page-container">
+          <div className="product-page-boxes">
+            <nav className="product-nav">
+              <ul>
+                <li
+                  className="product-sub-menu"
+                  onClick={() => this.isSelected(1)}
                 >
-                  <li>
-                    <a href="#">2.5cm屏風</a>
-                  </li>
-                  <li>
-                    <a href="#">桌上屏風</a>
-                  </li>
-                </ul>
-              </li>
-              <li
-                className="product-sub-menu"
-                onClick={() => this.isSelected(2)}
-              >
-                <NavLink to="/product/chairs">
-                  椅子系列<div className="fa fa-caret-down right"></div>
-                </NavLink>
-                <ul
-                  className={`collapsible ${
-                    this.state.active === 2 ? "open" : null
-                  }`}
+                  <NavLink to="/product/screens">
+                    屏風 Screens<div className="fa fa-caret-down right"></div>
+                  </NavLink>
+                  <ul
+                    className={`collapsible ${
+                      this.state.active === 1 ? "open" : null
+                    }`}
+                  >
+                    <li>
+                      <a href="#">2.5cm屏風</a>
+                    </li>
+                    <li>
+                      <a href="#">桌上屏風</a>
+                    </li>
+                  </ul>
+                </li>
+                <li
+                  className="product-sub-menu"
+                  onClick={() => this.isSelected(2)}
                 >
-                  <li>
-                    <a href="#">折合椅</a>
-                  </li>
-                  <li>
-                    <a href="#">排椅</a>
-                  </li>
-                </ul>
-              </li>
-              <li
-                className="product-sub-menu"
-                onClick={() => this.isSelected(3)}
-              >
-                <NavLink to="/product/desks">
-                  桌子系列<div className="fa fa-caret-down right"></div>
-                </NavLink>
-                <ul
-                  className={`collapsible ${
-                    this.state.active === 3 ? "open" : null
-                  }`}
+                  <NavLink to="/product/chairs">
+                    椅子系列 Chairs<div className="fa fa-caret-down right"></div>
+                  </NavLink>
+                  <ul
+                    className={`collapsible ${
+                      this.state.active === 2 ? "open" : null
+                    }`}
+                  >
+                    <li>
+                      <a href="#">折合椅</a>
+                    </li>
+                    <li>
+                      <a href="#">排椅</a>
+                    </li>
+                  </ul>
+                </li>
+                <li
+                  className="product-sub-menu"
+                  onClick={() => this.isSelected(3)}
                 >
-                  <li>
-                    <a href="#">主管桌</a>
-                  </li>
-                  <li>
-                    <a href="#">會議桌</a>
-                  </li>
-                  <li>
-                    <a href="#">辦公桌</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a href="#">工作站</a>
-              </li>
-            </ul>
-          </nav>
-        <Switch>
-          <Route path="/product/screens">
-            <Screenseries />
-          </Route>
-          <Route path="/product/chairs">
-            <Chairseries />
-          </Route>
-          <Route path="/product/desks">
-            <Deskseries />
-          </Route>
-        </Switch>
+                  <NavLink to="/product/desks">
+                    桌子系列 Desks<div className="fa fa-caret-down right"></div>
+                  </NavLink>
+                  <ul
+                    className={`collapsible ${
+                      this.state.active === 3 ? "open" : null
+                    }`}
+                  >
+                    <li>
+                      <a href="#">主管桌</a>
+                    </li>
+                    <li>
+                      <a href="#">會議桌</a>
+                    </li>
+                    <li>
+                      <a href="#">辦公桌</a>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#">工作站</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="product-page-boxes">
+            <Switch>
+              <Route path="/product/screens">
+                <Screenseries />
+              </Route>
+              <Route path="/product/chairs">
+                <Chairseries />
+              </Route>
+              <Route path="/product/desks">
+                <Deskseries />
+              </Route>
+            </Switch>
+          </div>
+        </div>
       </React.Fragment>
     );
   }
