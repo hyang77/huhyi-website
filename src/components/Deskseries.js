45be1d33ef3da9b5f33b6528d17a1c3b18e62f38
Haoli Yang <hyang77@hawk.iit.edu>
2020-05-19 11:10:18 +0800

    modify css style

--- src/components/Deskseries.js
+++ src/components/Deskseries.js
@@ -3,8 +3,8 @@ import React from "react";
 function Deskseries() {
   return (
     <React.Fragment>
-      <div className="desk-title">桌子系列</div>
-      <div className="desk-items">這裡顯示所有桌子產品照片(分類劃分)</div>
+      <div className="title desk-title">桌子系列</div>
+      <div className="items desk-items">這裡顯示所有桌子產品照片(分類劃分)</div>
     </React.Fragment>
   );
 }
--- src/components/Screenseries.js
+++ src/components/Screenseries.js
@@ -3,8 +3,8 @@ import React from "react";
 function Screenseries() {
   return (
     <React.Fragment>
-      <div className="screen-title">屏風</div>
-      <div className="screen-items">這裡顯示所有屏風產品照片(分類劃分)</div>
+      <div className="title screen-title">屏風</div>
+      <div className="items screen-items">這裡顯示所有屏風產品照片(分類劃分)</div>
     </React.Fragment>
   );
 }
